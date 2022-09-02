8d9552dcd0fdf9aaa401c2d94c221ccdbe93e881
francoC1410 <111617652+francoC1410@users.noreply.github.com>
2022-09-01 21:48:16 -0300

    Add files via upload

---
+++ Ejercicio1-java/src/Tiposdedatos/Tipos.java
@@ -0,0 +1,45 @@
+package Tiposdedatos;
+
+public class Tipos {
+
+    public static void main(String[] args) {
+        //Variables tipos primitivos
+
+        //1. Numeros
+
+        //1.1 Enteros
+
+        byte variable1 = 1;
+        short variable2 = 10;
+        int variable3 = 50;
+        long variable4 = 100;
+
+        System.out.println(variable1);
+        System.out.println(variable2);
+        System.out.println(variable3);
+        System.out.println(variable4);
+
+        //1.2 Decimales
+
+        float variable5 = 1.5f;
+        double variable6 = 20.2d;
+
+        System.out.println(variable5);
+        System.out.println(variable6);
+        //2 Booleano
+
+        boolean variable7 = true;
+        boolean variable8 = false;
+
+        System.out.println(variable7);
+        System.out.println(variable8);
+
+        //3. Texto
+
+        char variable9 = 'a';
+        String variable10 = "Hola mundo de OpenBootcamp";
+
+        System.out.println(variable9);
+        System.out.println(variable10);
+    }
+}
